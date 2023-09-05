d613b5395e249335573f45782a5d0de0aee825df
shivam755 <www.shivam567@gmail.com>
2023-09-05 17:24:39 +0530

    adding user related files

---
+++ backend/Models/Users.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
